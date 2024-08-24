0dda4d33cc53bb49e9afb2d9ca91683f1ce9281d
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-08-24 14:27:15 +0900

    ドキュメントを生成する処理を追加した。

--- Example/Func.cs
+++ Example/Func.cs
@@ -1,8 +1,23 @@
 ﻿namespace Example;
 
+/// <summary>
+/// Arithmetics function.
+/// </summary>
 public class Func
 {
+    /// <summary>
+    /// Addition operation.
+    /// </summary>
+    /// <param name="a">Operand 1.</param>
+    /// <param name="b">Operand 2.</param>
+    /// <returns>Result.</returns>
     public static int Add(int a, int b) => a + b;
 
+    /// <summary>
+    /// Subtraction operation.
+    /// </summary>
+    /// <param name="a">Operand 1.</param>
+    /// <param name="b">Operand 2.</param>
+    /// <returns>Result.</returns>
     public static int Sub(int a, int b) => a - b;
 }
